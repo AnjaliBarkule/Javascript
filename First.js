0f9cb3297a7590ac0eb589eb9975bb3445d5aa8a
AnjaliBarkule <barkuleanjali2603@gmail.com>
2025-09-07 20:34:59 +0530

    First commit

---
+++ First.js
@@ -0,0 +1,12 @@
+
+var num=20;//num+1=>
+
+var num1="20";// num1+1=>
+
+    console.log(num+1); //21
+    console.log(num1+1); //201
+    console.log(20=="20");
+    console.log(20==="20");
+
+    // console.log(typeof(num));
+    // console.log(typeof(num1));
